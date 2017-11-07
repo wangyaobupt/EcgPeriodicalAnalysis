caf036f123491bee3be9f82cbd3167082991a8fe
Yao Wang <wangyao_bupt@hotmail.com>
2017-11-07 17:15:32 +0800

    add more friendly interface

--- DataReaderMITBIH.py
+++ DataReaderMITBIH.py
@@ -1,10 +1,12 @@
+import os
+
 class DataReaderMITBIH:
   def __init__(self, filename, sampleRate):
     self.filename = filename
     self.sampleRate = sampleRate
 
-  def readRriListFromFile(self):
-    with open(self.filename, 'r') as f:
+  def readRriListFromFile(self, filename):
+    with open(filename, 'r') as f:
       r_peak_pos_list = f.read().split(",")
     r_peak_pos_list = [x for x in r_peak_pos_list if len(x.strip()) > 0]
     r_peak_pos_list = map(int, r_peak_pos_list)
@@ -19,16 +21,27 @@ class DataReaderMITBIH:
 
     return rri_list
 
-  def prepareDataSet(self, time_steps_for_rnn, readFromFile=False):
-    dataSet = []
-    rriList = self.readRriListFromFile()
+  def prepareDataSet(self, time_steps_for_rnn):
+    result = []
+    if os.path.isfile(self.filename):
+      result = self.readDataSetFromOneFile(self.filename, time_steps_for_rnn)
+    elif os.path.isdir(self.filename):
+      for root, dirs, filenames in os.walk(self.filename):
+        for file in filenames:
+          if 'Rpeak.txt' in file:
+            result += self.readDataSetFromOneFile(os.path.join(root, file), time_steps_for_rnn)
 
+    return result
+
+  def readDataSetFromOneFile(self, filename, time_steps_for_rnn):
+    dataSet = []
+    rriList = self.readRriListFromFile(filename)
     idx = 0
-    while (idx+time_steps_for_rnn) < len(rriList):
-      dataList = [x for x in rriList[idx:idx+time_steps_for_rnn]]
-      label = rriList[idx+time_steps_for_rnn]
-      dataSet.append({'data':dataList, 'label': label})
-      idx=idx+1
+    while (idx + time_steps_for_rnn) < len(rriList):
+      dataList = [x for x in rriList[idx:idx + time_steps_for_rnn]]
+      label = rriList[idx + time_steps_for_rnn]
+      dataSet.append({'data': dataList, 'label': label})
+      idx = idx + 1
 
     return dataSet
 
